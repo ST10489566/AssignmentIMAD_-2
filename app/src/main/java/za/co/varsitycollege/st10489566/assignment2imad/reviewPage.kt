fa8d2d3b90773256632bb81b12809e9828d651b0
ST10489566 <ST10489566@vcconnect.edu.za>
2025-05-22 13:51:21 +0200

    Review Screen

--- app/src/main/java/za/co/varsitycollege/st10489566/assignment2imad/reviewPage.kt
+++ app/src/main/java/za/co/varsitycollege/st10489566/assignment2imad/reviewPage.kt
@@ -1,6 +1,7 @@
 package za.co.varsitycollege.st10489566.assignment2imad
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -14,6 +15,12 @@ class reviewPage : AppCompatActivity() {
         setContentView(R.layout.activity_review_page)
 
         val scoreTextView = findViewById<TextView>(R.id.scoreTextView)
+        val ReviewTextView = findViewById<TextView>(R.id.ReviewTextView)
+        val rtrnButton = findViewById<Button>(R.id.rtrnButton)
+        val butt....
+
+        val score = intent.getIntExtra("score",0)
+        scoreTextView.text = "Your final scor: $score /5"
 
 
 
